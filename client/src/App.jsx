9717bfacc7f0ecce5986794d6b32dcc61a48e89f
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-10-29 21:11:46 -0400

    feat: add BrowserRouter and useEffect for fetching user from API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,55 @@
+import { useEffect, useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Header from "./components/Header";
+import Login from "./pages/Login";
+
 function App() {
-    return <h1 className="text-3xl font-bold underline">CodePath Capstone</h1>;
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const getUser = async () => {
+            try {
+                const response = await fetch("/api/auth/login/success", {
+                    method: "GET",
+                    credentials: "include",
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Credentials": true,
+                    },
+                });
+
+                if (response.ok) {
+                    const resObject = await response.json();
+                    setUser(resObject.user);
+                } else {
+                    throw new Error("Authentication has failed!");
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        getUser();
+    }, []);
+
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: (
+                <div className="h-screen w-full">
+                    <Header user={user} />
+                </div>
+            ),
+        },
+        {
+            path: "/login",
+            element: <Login />,
+        },
+    ]);
+
+    return <RouterProvider router={router} />;
 }
 
 export default App;
