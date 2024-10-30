e6f609c9fc65d5958437ae6a460a9a8a3d5779da
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-10-30 16:54:52 -0400

    refactor: update routes to use UserProvider and useUser hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,16 @@
-import { useEffect, useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import { UserProvider } from "./context/UserContext";
 import Header from "./components/Header";
 import Login from "./pages/Login";
 
 function App() {
-    const [user, setUser] = useState(null);
-
-    useEffect(() => {
-        const getUser = async () => {
-            try {
-                const response = await fetch("/api/auth/login/success", {
-                    method: "GET",
-                    credentials: "include",
-                    headers: {
-                        Accept: "application/json",
-                        "Content-Type": "application/json",
-                        "Access-Control-Allow-Credentials": true,
-                    },
-                });
-
-                if (response.ok) {
-                    const resObject = await response.json();
-                    setUser(resObject.user);
-                } else {
-                    throw new Error("Authentication has failed!");
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        getUser();
-    }, []);
-
     const router = createBrowserRouter([
         {
             path: "/",
             element: (
                 <div className="h-screen w-full">
-                    <Header user={user} />
+                    <Header />
                 </div>
             ),
         },
@@ -49,7 +20,11 @@ function App() {
         },
     ]);
 
-    return <RouterProvider router={router} />;
+    return (
+        <UserProvider>
+            <RouterProvider router={router} />
+        </UserProvider>
+    );
 }
 
 export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,26 +1,34 @@
-import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 
-const Header = ({ user }) => {
+import { useUser } from "../hooks/useUser";
+
+const Header = () => {
+    const { user } = useUser();
+
     return (
-        <div className="flex w-full flex-row justify-end border-b border-gray-200 px-24 py-6">
-            {user ? <LogoutButton /> : <LoginButton />}
-            {user && user.img && <img src={user.img} alt="User Avatar" className="ml-4 h-10 w-10 rounded-full" />}
+        <div className="flex w-full flex-row items-center justify-between border-b border-gray-200 px-24 py-6">
+            <div className="">
+                <Link to="/matches">Matches</Link>
+            </div>
+            <div className="flex flex-row items-center justify-center">
+                {user ? <LogoutButton /> : <LoginButton />}
+                {user && user.img && (
+                    <img
+                        src={user.img}
+                        alt="User Avatar"
+                        className="ml-4 h-10 w-10 rounded-full"
+                        onError={(e) => {
+                            e.target.onerror = null;
+                            e.target.src = `https://ui-avatars.com/api/?name=${user.username}`;
+                        }}
+                    />
+                )}
+            </div>
         </div>
     );
-};
-
-Header.propTypes = {
-    user: PropTypes.shape({
-        user_id: PropTypes.number.isRequired,
-        username: PropTypes.string.isRequired,
-        img: PropTypes.string.isRequired,
-    }),
-};
-
-Header.defaultProps = {
-    user: null,
 };
 
 export default Header;
