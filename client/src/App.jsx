a3aaf563dbe84bfa32cb58c78e7e326d5367a5f6
jawadrada <jawadrada18@gmail.com>
2024-10-30 13:55:14 -0400

    feat(routes): add new routes for app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Fallback from "./pages/Fallback";
+
 function App() {
-    return <h1 className="text-3xl font-bold underline">CodePath Capstone</h1>;
+    return (
+        <>
+            <Router>
+                <div className="App">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/signin" element={<Signin />} />
+                        <Route path="/signup" element={<Signup />} />  
+                        <Route path="*" element={<Fallback />} />
+                    </Routes>
+                </div>
+            </Router>
+        </>
+    );
 }
 
 export default App;
