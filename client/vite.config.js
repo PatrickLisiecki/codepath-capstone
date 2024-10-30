3bb7c6ebc49f21e66d59dd07c3df138a9896d9cb
PatrickLisiecki <impatricklisiecki@gmail.com>
2024-10-29 21:03:43 -0400

    refactor(vite): modify Vite config to use /api as the base path instead of /api/v1

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
         port: 5173,
         proxy: {
-            "/api/v1": {
+            "/api": {
                 target: "http://localhost:3000",
                 changeOrigin: true,
             },
